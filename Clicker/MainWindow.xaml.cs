af844f26ca3fc02e2169b841e2197e0b89bd1eb9
gmbl3r <savchukdev@gmail.com>
2019-07-11 11:54:51 +0300

    Ability to change shortcur.

--- Clicker/MainWindow.xaml.cs
+++ Clicker/MainWindow.xaml.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Text;
 using System.Windows;
 using System.Runtime.InteropServices;
 using System.Windows.Interop;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Linq;
 
 namespace Clicker
 {
@@ -23,14 +27,12 @@ namespace Clicker
             InitializeComponent();
         }
 
-        //Modifiers:
-        private const uint MOD_NONE = 0x0000; //(none)
-        private const uint MOD_ALT = 0x0001; //ALT
-        private const uint MOD_CONTROL = 0x0002; //CTRL
-        private const uint MOD_SHIFT = 0x0004; //SHIFT
-        private const uint MOD_WIN = 0x0008; //WINDOWS
-        //CAPS LOCK:
-        private const uint VK_CAPITAL = 0x46;
+        private Key[] modifiers = {
+          Key.LeftCtrl, Key.RightCtrl,
+          Key.LeftAlt, Key.RightAlt,
+          Key.LeftShift, Key.RightShift,
+          Key.LWin, Key.RWin, Key.System
+        };
 
         private IntPtr _windowHandle;
         private HwndSource _source;
@@ -42,7 +44,9 @@ namespace Clicker
             _source = HwndSource.FromHwnd(_windowHandle);
             _source.AddHook(HwndHook);
 
-            RegisterHotKey(_windowHandle, HOTKEY_ID, MOD_SHIFT, VK_CAPITAL); //CTRL + CAPS_LOCK
+            shorcut.Text = Properties.Settings.Default["Text"].ToString();
+
+            map_buttons();
         }
 
         private IntPtr HwndHook(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
@@ -55,7 +59,7 @@ namespace Clicker
                     {
                         case HOTKEY_ID:
                             int vkey = (((int)lParam >> 16) & 0xFFFF);
-                            if (vkey == VK_CAPITAL)
+                            if (vkey == KeyInterop.VirtualKeyFromKey((Key)Properties.Settings.Default.Key))
                             {
                                 MouseOperations.MouseEvent(MouseOperations.MouseEventFlags.RightDown);
                             }
@@ -72,6 +76,60 @@ namespace Clicker
             _source.RemoveHook(HwndHook);
             UnregisterHotKey(_windowHandle, HOTKEY_ID);
             base.OnClosed(e);
+        }
+
+        private void TextBox_KeyUp(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (!this.modifiers.Contains(e.Key)) {
+                Properties.Settings.Default["Modifiers"] = e.KeyboardDevice.Modifiers.GetHashCode();
+                Properties.Settings.Default["Key"] = e.Key.GetHashCode();
+                Properties.Settings.Default["Text"] = build_text(e);
+                Properties.Settings.Default.Save();
+
+                shorcut.Text = build_text(e);
+
+                grid.Focus();
+
+                map_buttons();
+            }
+            
+        }
+
+        private void TextBox_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (!this.modifiers.Contains(e.Key))
+            {
+                shorcut.Text = build_text(e);
+            }
+        }
+
+        private String build_text(KeyEventArgs e)
+        {
+            StringBuilder builder = new StringBuilder();
+
+            builder.Append(e.KeyboardDevice.Modifiers.ToString());
+            builder.Append(" + ");
+            builder.Append(e.Key.ToString());
+
+            return builder.ToString();
+        }
+
+        private void map_buttons()
+        {
+            uint modifiers = (uint)Properties.Settings.Default.Modifiers;
+            uint key = (uint)KeyInterop.VirtualKeyFromKey((Key)Properties.Settings.Default.Key);
+
+            RegisterHotKey(_windowHandle, HOTKEY_ID, modifiers, key);
+        }
+
+        private void Shorcut_GotFocus(object sender, RoutedEventArgs e)
+        {
+            shorcut.Background = Brushes.LightYellow;
+        }
+
+        private void Shorcut_LostFocus(object sender, RoutedEventArgs e)
+        {
+            shorcut.Background = Brushes.White;
         }
     }
 }
--- Clicker/Properties/Settings.Designer.cs
+++ Clicker/Properties/Settings.Designer.cs
@@ -8,22 +8,54 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Clicker.Properties
-{
-
-
+namespace Clicker.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.1.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("6")]
+        public int Modifiers {
+            get {
+                return ((int)(this["Modifiers"]));
+            }
+            set {
+                this["Modifiers"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("49")]
+        public int Key {
+            get {
+                return ((int)(this["Key"]));
+            }
+            set {
+                this["Key"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("Control, Shift + F")]
+        public string Text {
+            get {
+                return ((string)(this["Text"]));
+            }
+            set {
+                this["Text"] = value;
             }
         }
     }
